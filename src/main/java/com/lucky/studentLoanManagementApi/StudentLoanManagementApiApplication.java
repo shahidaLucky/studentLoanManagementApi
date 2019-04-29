b195b848539260a5916d61e0535aabb09fee9d4a
shahidaLucky <shahidakawsar@gmail.com>
2019-04-30 02:24:02 +0600

    Initial Commit.

---
+++ src/main/java/com/lucky/studentLoanManagementApi/StudentLoanManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.lucky.studentLoanManagementApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentLoanManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentLoanManagementApiApplication.class, args);
+	}
+
+}
